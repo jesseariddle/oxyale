92926352e595c05cfe398e5735b9def3eca2f973
Jesse Riddle <jesse@orkey.net>
2014-04-09 08:09:18 -0400

    include a built-in client with the library

---
+++ src/ox_client.c
@@ -0,0 +1,6 @@
+#include "env.h"
+#include "ox_client.h"
+
+void ox_client_init( ox_client_t *client )
+{
+}
---
+++ src/ox_client.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef OX_CLIENT_H_
+#define OX_CLIENT_H_
+
+typedef struct {
+    
+} ox_client_t;
+
+void ox_client_init( ox_client_t *client );
+
+#endif /* OX_CLIENT_H_ */
