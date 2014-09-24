6a3f0ab6c32ffd881098cadd9a2b2e8e0ecbdeea
Jesse Riddle <jesse@orkey.net>
2014-09-23 23:49:19 -0400

    derp

--- src/ox_client.c
+++ src/ox_client.c
@@ -13,14 +13,14 @@ void ox_client_connect(ox_client_t *client, const char *username, const char *ho
 {
     char *ipv4addr = ox_net_resolve_host(host);
 
-	uv_connect_t *connect = malloc(sizeof uv_connect_t);
-	struct sockaddr_in dest = uv_ipv4_addr("127.0.0.1", 80);
-	uv_tcp_connect(connect, socket, dest, ox_client_on_connect);
+    uv_connect_t *connect = malloc(sizeof uv_connect_t);
+    struct sockaddr_in dest = uv_ipv4_addr("127.0.0.1", 80);
+    uv_tcp_connect(connect, socket, dest, ox_client_on_connect);
 }
 
 void ox_client_on_connect(uv_stream_t *client, int status)
 {
-
+    
 }
 
 void ox_client_disconnect(ox_client_t *client)
